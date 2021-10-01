24cbef474b96ad8bd57a4eec58cd6f67a744d7b5
MarekCzernohous <MarekCzernohous@0-209-LR>
2021-10-01 09:11:21 +0200

    Vertex finished

--- JavaWillNurSpielen/src/Main.java
+++ JavaWillNurSpielen/src/Main.java
@@ -2,23 +2,33 @@
 public class Main {
 
 	public static void main(String[] args) {
-
-		Vertex v1 = new Vertex(42, 34);
-		Vertex maus = new Vertex(3, 54);
-		Vertex v3 = v1.skalarMult(3.14);
-		Vertex katze = new Vertex(1,1);
-		Vertex elefant = katze.add(maus);
+ 
 		
 		
+		Vertex v1 = new Vertex(17, 4);
+		Vertex v2 = new Vertex(42, 23);
+		Vertex v3 = new Vertex(0,0);
+		Vertex v4 = new Vertex(42, 23);
 		
-		System.out.println(katze);
-		katze.addMod(maus);
-		System.out.println(katze);
-		System.out.println(elefant);
+		System.out.println(v2.toString());
+		System.out.println(v2);
+		System.out.println(v1.length());
+		System.out.println(v2.length());
+		System.out.println(v3.length());
+		System.out.println(v1.skalarMult(0.8));
+		System.out.println(v1);
+		System.out.println(v1.add(v2));
+		System.out.println(v1);
+		System.out.println(v2);
+		System.out.println(v1.equals(v2));
+		System.out.println(v2.equals(v3));	
+		System.out.println(v2.equals(v4));
+		System.out.println(v4.equals(v2));	
+		
+		
 
 	
-		
-	//	System.out.println(v1.length());
+	
 	
 
 
--- JavaWillNurSpielen/src/Vertex.java
+++ JavaWillNurSpielen/src/Vertex.java
@@ -32,9 +32,9 @@ public class Vertex {
 		return new Vertex(x + v2.x, y + v2.y);
 	}
 
-	public void addMod(Vertex hund) {
-		x = x + hund.x;
-		y = y + hund.y;
+	public void addMod(Vertex v2) {
+		x = x + v2.x;
+		y = y + v2.y;
 	}
 
 	public void setX(double x) {
@@ -52,4 +52,32 @@ public class Vertex {
 	public double getY() {
 		return y;
 	}
+    
+	public boolean equals(Object thatObject) {
+		if(thatObject instanceof Vertex) {
+			Vertex that = (Vertex)thatObject;
+			return this.x==that.x && this.y == that.y;
+		}
+		return false;
+	}
+	
+	public Vertex sub(Vertex v2) {
+		return new Vertex(x - v2.x, y - v2.y);
+	}
+	
+	public double distance(Vertex v2) {
+		double a = v2.x - this.x;
+		double b = v2.y - this.y;
+
+		return Math.sqrt(a * a + b * b);
+	}
+
+	public void normalize(){
+		double factor =  this.length();
+		
+		this.x=x/factor;
+		this.y=y/factor;
+	
+	}
+	
 }
