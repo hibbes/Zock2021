4ed62fc86235961136d1b36c43b9906973830faf
MarekCzernohous <MarekCzernohous@0-209-LR>
2021-11-23 14:46:12 +0100

    FIrstPanel

---
+++ JavaWillNurSpielen/src/FirstPanel.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+
+import javax.swing.*;
+
+
+public class FirstPanel extends JPanel{
+	
+@Override public void paintComponent(java.awt.Graphics g) {
+	g.setColor(Color.red);
+	g.fillRect(30,50,45,80);
+	
+}
+	
+	
+	
+
+}
--- JavaWillNurSpielen/src/Main.java
+++ JavaWillNurSpielen/src/Main.java
@@ -1,20 +1,14 @@
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
  
-		Vertex v1 = new Vertex(5,5);
-		GeometricObject o1 = new GeometricObject(17,4, new Vertex(42,23));
-		GeometricObject o2 = new GeometricObject(17,4);
-		GeometricObject o3 = new GeometricObject(17);
-		GeometricObject o4 = new GeometricObject();
-		
-		System.out.println(o1.equals(o2));
-		
-		o2.moveTo(42,23);
-		System.out.println(o1.equals(o2));
-		System.out.println(o2.contains(v1));
-		System.out.println(o4);
+		JFrame f=new JFrame("Mein erstes Rechteck");
+		f.add(new FirstPanel());
+		f.pack();
+		f.setVisible(true);
+
 		
 		
 		
