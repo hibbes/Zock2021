d9f983646d304b3f5c489e8635fd3894c60b9bee
MarekCzernohous <MarekCzernohous@0-208-Lehrer>
2021-11-24 09:16:20 +0100

    Bugfixes

--- JavaWillNurSpielen/src/SimpleSizedPanel.java
+++ JavaWillNurSpielen/src/SimpleSizedPanel.java
@@ -4,6 +4,7 @@ public class SimpleSizedPanel extends SizedPanel {
 
 	public void paintComponent(java.awt.Graphics g) {
 		g.fillRect(30,50,45,80);
+		g.fillOval(40, 60, 90, 30);
 	}
 	
 	public static void main(String[] args) {
