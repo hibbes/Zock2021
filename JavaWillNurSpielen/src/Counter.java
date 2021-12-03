dbd2eeb796c3c0789d175b4267147cb904cf2b23
MarekCzernohous <MarekCzernohous@ALZi-Marek>
2021-12-03 09:35:38 +0100

    Aufgabe 23

--- JavaWillNurSpielen/src/Counter.java
+++ JavaWillNurSpielen/src/Counter.java
@@ -5,21 +5,51 @@ import util.ShowInFrame;
 import java.awt.event.*;
 
 public class Counter extends SimpleButton {
-int i = 0;
+static int i = 0;
 	public Counter() {
 		b1.addActionListener(
 
 				new ActionListener() {
-					int i = 0;
-
+					
 					public void actionPerformed(ActionEvent e) {
 						i++;
 						l.setText("" + i);
+						
 					}
 				}
 
 		);
+		
+		b2.addActionListener(
+
+				new ActionListener() {
+					
+
+					public void actionPerformed(ActionEvent e) {
+						i--;
+						l.setText("" + i);
+						
+					}
+				}
+
+		);
+		
+		b3.addActionListener(
+
+				new ActionListener() {
+					
+
+					public void actionPerformed(ActionEvent e) {
+						i=0;
+						l.setText("" + i);
+						
+					}
+				}
+
+		);
+		
 	}
+	
 
 	public static void main(String[] args) {
 		ShowInFrame.show(new Counter());
