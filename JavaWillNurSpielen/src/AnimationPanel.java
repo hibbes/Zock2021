36b7f2dc04293775ab482097b969d01eade6bb5d
MarekCzernohous <MarekCzernohous@ALZi-Marek>
2021-12-08 09:55:10 +0100

    RemoveAnimation-Fix

--- JavaWillNurSpielen/src/AnimationPanel.java
+++ JavaWillNurSpielen/src/AnimationPanel.java
@@ -29,7 +29,7 @@ class AnimationPanel extends SimpleAnimation {
 	public static void main(String[] args) {
 		MovablePanel p = new AnimationPanel();
 		p.gos.add(new MovableImage("hexe.png", 0, 0, 1, 1));
-		p.gos.add(new MovableImage("biene.png", 800, 800, -1, -1));
+		p.gos.add(new MovableImage("biene.png", 600, 600, -1, -1));
 		ShowInFrame.show(p);
 	}
 }
--- JavaWillNurSpielen/src/SizedPanel.java
+++ JavaWillNurSpielen/src/SizedPanel.java
@@ -5,8 +5,8 @@ import util.ShowInFrame;
 import java.awt.Dimension;
 public class SizedPanel extends JPanel{
 
-	int preferredWidth=1024;
-	int preferredHeight=768;
+	int preferredWidth=1500;
+	int preferredHeight=1000;
 	
 	public SizedPanel(int w, int h) {
 		preferredWidth=w;
