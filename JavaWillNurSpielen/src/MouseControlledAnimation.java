3cc8d7b0762d318c35a41f289d0114a21b4c3a5d
MarekCzernohous <MarekCzernohous@0-209-LR>
2021-12-10 08:24:25 +0100

    MouseControlledAnimation

---
+++ JavaWillNurSpielen/src/MouseControlledAnimation.java
@@ -0,0 +1,31 @@
+import util.*;
+import java.awt.event.*;
+
+public class MouseControlledAnimation extends RemoveAnimation{
+	
+GameObject controlled;
+
+MouseControlledAnimation(final GameObject contr){
+	controlled=contr;
+	gos.add(controlled);
+	addMouseListener(new MouseAdapter() {
+		public void MouseClicked(MouseEvent e) {
+			final Vertex delta = new Vertex(e.getX(), e.getY());
+			delta.addMod(controlled.getPos().skalarMult(-1));
+			delta.addMod(new Vertex(-controlled.getWidth()/2,-controlled.getHeight()/2));
+		    final double factor = controlled.getDeltaPos().length()/delta.length();
+		    delta.skalarMultMod(factor);
+		    controlled.setDeltaPos(delta);
+		}
+	});
+}
+	
+	
+	public static void main(String[] args) {
+		MovablePanel p = new MouseControlledAnimation(new MovableImage("hexe.png", 0, 0, 1, 1));
+		p.gos.add(new MovableImage("biene.png", 600, 600, -1, -1));
+		ShowInFrame.show(p);
+
+	}
+
+}
